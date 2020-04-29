fb79b752afee88f4262f9443333631ab34c08661
bossach <boss52@inbox.ru>
2020-04-29 15:53:41 +0300

    Task 2

---
+++ src/lesson_1/Task_2.java
@@ -0,0 +1,22 @@
+package lesson_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task_2 {
+    public static void main(String[] args) {
+
+        String[] array = {"one", "two", "three"};
+        //Integer[] array = {1, 2, 3};
+        List list = toArrayList(array);
+
+        System.out.println(list.get(0).getClass());
+        System.out.println(list);
+
+    }
+
+    public static <T> ArrayList<T> toArrayList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
+}
