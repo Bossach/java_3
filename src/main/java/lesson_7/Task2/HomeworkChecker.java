ebc092e9f244acd3c3fed54a5b8c22d9f1c8ac10
bossach <boss52@inbox.ru>
2020-05-27 23:15:48 +0300

    lesson_7 Task2 to be continued

--- src/main/java/lesson_7/Task2/HomeworkChecker.java
+++ src/main/java/lesson_7/Task2/HomeworkChecker.java
@@ -7,69 +7,170 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 
 public class HomeworkChecker {
 
 
-    private List<Task> checkers;
-    private List<File> works;
+    private List<Task> tasks;
     private String dir;
 
-    public HomeworkChecker(String workDirectory, Task[] tasks) {
+    public HomeworkChecker(String workDirectory, List<Task> tasks) {
         this.dir = workDirectory;
+        this.tasks = tasks;
     }
 
     public static void main(String[] args) {
+        final String PATH = "files/lesson_7/HomeworksToCheck";
+        //final boolean verboseOutput = false;
+
+
         List<Task> taskList = new ArrayList<>();
-        taskList.add(new Task("calculate", int.class, new Class[]{int.class, int.class, int.class, int.class},
-                new Predicate<>() {
-                    @Override
-                    public boolean test(Object o) {
-                        if (o = )
-                        return false;
-                    }
-                }, new Integer[][]{}));
+
+        Task currentTask = new Task("calculate", int.class, new Class[]{int.class, int.class, int.class, int.class},
+                o -> {
+                    //prepare data
+                    Object[] arr = (Object[]) o;
+
+                    int expectedValue = (int) ((Object[]) arr[0])[0];
+
+                    int resultValue = (int) arr[1];
+                    //
+                    //there is checks
+                    return resultValue == expectedValue;
+                }, new Integer[][][]{
+                        //test data
+                        //{{expected Return Value},  {parameters...}},{...},{...}
+                        {{15}, {5, 1, 4, 2}},
+                        {{21}, {3, 2, 15, 3}}
+        });
+
+        currentTask.setDescription("int");
+
+        taskList.add(currentTask);
+
+        currentTask = new Task("calculate", float.class, new Class[]{float.class, float.class, float.class, float.class},
+                o -> {
+                    //prepare data
+                    Object[] arr = (Object[]) o;
+
+                    float expectedValue = (float) ((Object[]) arr[0])[0];
+
+                    float resultValue = (float) arr[1];
+                    //
+                    //there is checks
+                    return resultValue == expectedValue;
+                }, new Float[][][]{
+                //test data
+                //{{expected Return Value},  {parameters...}},{...},{...}
+                {{13.5f}, {3f, 2f, 5f, 2f}},
+                {{17f}, {4f, 0.5f, 15f, 4f}}
+        });
+
+        currentTask.setDescription("float");
+
+        taskList.add(currentTask);
+
+        currentTask = new Task("checkTwoNumbers", boolean.class, new Class[]{int.class, int.class},
+                o -> {
+                    //prepare data
+                    Object[] arr = (Object[]) o;
+
+                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
+
+                    boolean resultValue = (boolean) arr[1];
+                    //
+                    //there is checks
+                    return resultValue == expectedValue;
+                }, new Object[][][]{
+                //test data
+                //{{expected Return Value},  {parameters...}},{...},{...}
+                {{true}, {7, 9}},
+                {{false}, {1, 3}},
+                {{false}, {15, 30}}
+        });
+        taskList.add(currentTask);
+
+        currentTask = new Task("isNegative", boolean.class, new Class[]{int.class},
+                o -> {
+                    //prepare data
+                    Object[] arr = (Object[]) o;
+
+                    boolean resultValue = (boolean) arr[1];
+
+                    int argument = (int) ((Object[]) arr[2])[0];
+                    //
+                    //there is checks
+                    boolean expectedResult = argument < 0;
+
+                    return expectedResult == resultValue;
+                }, new Object[][][]{
+                //test data
+                //{{expected Return Value (if need)},  {parameters...}},{...},{...}
+                {{}, {7}},
+                {{}, {-5}},
+                {{}, {0}}
+        });
+
+        taskList.add(currentTask);
+
+        currentTask = new Task("isLeapYear", boolean.class, new Class[]{int.class},
+                o -> {
+                    //prepare data
+                    Object[] arr = (Object[]) o;
+
+                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
+
+                    boolean resultValue = (boolean) arr[1];
+                    //
+                    //there is checks
+                    return resultValue == expectedValue;
+                }, new Object[][][]{
+                //test data
+                //{{expected Return Value},  {parameters...}},{...},{...}
+                {{true}, {2000}},
+                {{false}, {2001}},
+                {{false}, {2600}}
+        });
+
+        taskList.add(currentTask);
 
 
+        HomeworkChecker checker = new HomeworkChecker(PATH, taskList);
 
-        HomeworkChecker checker = new HomeworkChecker("target/classes/lesson_7/Task2/HomeworksToCheck");
-
-        checker.startCheck();
+        checker.checkHomeworks();
     }
 
-    public void startCheck() {
-
+    public void checkHomeworks() {
         File fDir = new File(dir);
 
-        works = new ArrayList<>();
-
         for (String filename : Objects.requireNonNull(fDir.list((file, s) -> s.endsWith(".class")))) {
-            check(filename);
+            checkHomework(filename);
         }
     }
 
-    private void check(String filename) {
+    private void checkHomework(String filename) {
 
         //
         String className = filename.substring(0, filename.length() - 6);
-        String filePath = dir + "/" + filename;
+//        String filePath = dir + "/" + filename;
+        String filePath = dir;
         File file = new File(filePath);
         //
 
-
         Class<?> workClass;
         try {
-            workClass = URLClassLoader.newInstance(new URL[]{file.toURL()}).loadClass(className);
-            //Вылетает ClassNotFoundException. Путь коррекен, файл существует, проверял. Имя классса тоже совпадает
+            ClassLoader loader = new URLClassLoader(new URL[]{file.toURL()}, getClass().getClassLoader());
+            workClass = loader.loadClass(className);
         } catch (ClassNotFoundException | MalformedURLException e) {
             System.err.println(filename + " : can't load class");
             e.printStackTrace();
             return;
         }
 
-        for (Task checker : checkers) {
-            checker.check(workClass);
+        System.out.println(workClass.getName() + " :");
+
+        for (Task task : tasks) {
+            task.checkTask(workClass);
         }
 
     }
--- src/main/java/lesson_7/Task2/Task.java
+++ src/main/java/lesson_7/Task2/Task.java
@@ -13,32 +13,33 @@ public class Task {
     public final Class<?> returnType;
     public final Class<?>[] parameterTypes;
     private final Predicate<Object> predicate;
-    private final List<Object[]> parametersSet;
+    private final List<Object[][]> dataSet;
+    private String description = "";
 
-    public Task(String supposedName, Class<?> returnType, Class<?>[] parameterTypes, Predicate<Object> predicate, Object[][] parameters) {
+    public Task(String supposedName, Class<?> returnType, Class<?>[] parameterTypes, Predicate<Object> predicate, Object[][][] data) {
         this.supposedName = supposedName;
         this.returnType = returnType;
         this.parameterTypes = parameterTypes;
         this.predicate = predicate;
-        parametersSet = Arrays.asList(parameters);
+        dataSet = Arrays.asList(data);
     }
 
-    public void check(Class<?> workClass) {
+    public void checkTask(Class<?> workClass) {
 
-        String name = workClass.getName().substring(0, workClass.getName().length() - 6);
+        List<Method> checkCandidates = filterMatchingMethods(workClass.getDeclaredMethods());
 
-        System.out.println(name + " :");
+        for (Method checkCandidate : checkCandidates) {
+            checkCandidate.setAccessible(true);
+        }
 
-        List<Method> checkCandidates = filter(workClass.getMethods());
-
-        System.out.println("\t" + supposedName + " : ");
+        System.out.print("\t" + supposedName + " " + description + " : ");
 
         String msg = "Failed or can't check!";
 
         //If any of candidates passed the task is passed
         for (Method method : checkCandidates) {
             try {
-                if (test(workClass, method)) {
+                if (checkMethod(workClass, method)) {
                     msg = "Passed!";
                     break;
                 }
@@ -49,19 +50,25 @@ public class Task {
         System.out.println(msg);
     }
 
-    public boolean test(Class<?> aClass, Method method) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        for (Object[] parameters : parametersSet) {
-            if (!singleTest(aClass, method, parameters))
+    public boolean checkMethod(Class<?> aClass, Method method) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        for (Object[] data : dataSet) {
+            if (!singleTest(aClass, method, data)) {
                 return false;
+            }
         }
         return true;
     }
 
-    private boolean singleTest(Class<?> aClass, Method method, Object[] parameters) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        return predicate.test(method.invoke(aClass.getConstructor().newInstance(), parameters));
+    private boolean singleTest(Class<?> aClass, Method method, Object[] data) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Object runObject = aClass.getConstructor().newInstance();
+        //Arr with 3 elems goin to test |
+        //-----------(1)expected return |value------(2)returns fact value----------(--this| is arguments array to invoke with them--)
+        //----------------------------- V----V(1)-----V(2)--------------------------------V--------V(3)Arguments array
+        Object testArgument = new Object[]{data[0], method.invoke(runObject,(Object[]) data[1]), data[1]};
+        return predicate.test(testArgument);
     }
 
-    private List<Method> filter(Method[] methods) {
+    private List<Method> filterMatchingMethods(Method[] methods) {
         List<Method> list = new ArrayList<>();
 
         //Find methods matching by arguments and return type
@@ -74,16 +81,17 @@ public class Task {
         for (Method method : list) {
             if (method.getName().equalsIgnoreCase(supposedName)) {
 
-                //TODO delete debug!!!
-                System.out.println("FoundByName! " + supposedName);
-
                 ArrayList<Method> nameMatchList = new ArrayList<>();
                 nameMatchList.add(method);
                 return nameMatchList;
             }
         }
 
-        //if name didnt found returns all matching by return type and parameters types
+        //if name didn't found returns all matching by return type and parameters types
         return list;
+    }
+
+    public void setDescription(String description) {
+        this.description = "(description: " + description + ")";
     }
 }
