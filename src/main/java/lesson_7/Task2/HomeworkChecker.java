b02d3b495101d43794b110608540686b8353dea8
bossach <boss52@inbox.ru>
2020-05-28 03:43:37 +0300

    lesson_7 HWchecker upd

--- src/main/java/lesson_7/Task2/HomeworkChecker.java
+++ src/main/java/lesson_7/Task2/HomeworkChecker.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,117 +22,7 @@ public class HomeworkChecker {
         final String PATH = "files/lesson_7/HomeworksToCheck";
         //final boolean verboseOutput = false;
 
-
-        List<Task> taskList = new ArrayList<>();
-
-        Task currentTask = new Task("calculate", int.class, new Class[]{int.class, int.class, int.class, int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    int expectedValue = (int) ((Object[]) arr[0])[0];
-
-                    int resultValue = (int) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
-                }, new Integer[][][]{
-                        //test data
-                        //{{expected Return Value},  {parameters...}},{...},{...}
-                        {{15}, {5, 1, 4, 2}},
-                        {{21}, {3, 2, 15, 3}}
-        });
-
-        currentTask.setDescription("int");
-
-        taskList.add(currentTask);
-
-        currentTask = new Task("calculate", float.class, new Class[]{float.class, float.class, float.class, float.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    float expectedValue = (float) ((Object[]) arr[0])[0];
-
-                    float resultValue = (float) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
-                }, new Float[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
-                {{13.5f}, {3f, 2f, 5f, 2f}},
-                {{17f}, {4f, 0.5f, 15f, 4f}}
-        });
-
-        currentTask.setDescription("float");
-
-        taskList.add(currentTask);
-
-        currentTask = new Task("checkTwoNumbers", boolean.class, new Class[]{int.class, int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
-
-                    boolean resultValue = (boolean) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
-                }, new Object[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
-                {{true}, {7, 9}},
-                {{false}, {1, 3}},
-                {{false}, {15, 30}}
-        });
-        taskList.add(currentTask);
-
-        currentTask = new Task("isNegative", boolean.class, new Class[]{int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean resultValue = (boolean) arr[1];
-
-                    int argument = (int) ((Object[]) arr[2])[0];
-                    //
-                    //there is checks
-                    boolean expectedResult = argument < 0;
-
-                    return expectedResult == resultValue;
-                }, new Object[][][]{
-                //test data
-                //{{expected Return Value (if need)},  {parameters...}},{...},{...}
-                {{}, {7}},
-                {{}, {-5}},
-                {{}, {0}}
-        });
-
-        taskList.add(currentTask);
-
-        currentTask = new Task("isLeapYear", boolean.class, new Class[]{int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
-
-                    boolean resultValue = (boolean) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
-                }, new Object[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
-                {{true}, {2000}},
-                {{false}, {2001}},
-                {{false}, {2600}}
-        });
-
-        taskList.add(currentTask);
-
+        List<Task> taskList = TaskList.getTasks();
 
         HomeworkChecker checker = new HomeworkChecker(PATH, taskList);
 
--- src/main/java/lesson_7/Task2/TaskList.java
+++ src/main/java/lesson_7/Task2/TaskList.java
@@ -4,20 +4,26 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskList {
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
-    @interface TaskAnnot {}
+    @interface TaskAnnot {
+    }
 
     /*
     Here is to write your tasks:
 
-    @TaskAnnot
-    public Task *anyMethodName*() { ... }
-
-    Method must return Task object
+    //Task template ------------------
+        @TaskAnnot
+        public Task *anyMethodName*() { ... }
+    //--------------------------------
+    Your method must return Task object
 
 
     Task constructor:
@@ -30,46 +36,55 @@ public class TaskList {
 
     (3) Class[]: * array of argument types in order *
 
-    (for example public int myMethod(int a, float b) --> new Task("myMethod", int.class, new Class[]{int.class, float.class},..,..))
+    (for example: public int myMethod(int a, float b) --> new Task("myMethod", int.class, new Class[]{int.class, float.class},..,..))
 
-    (4) Predicate: (o) -> { check logics returns boolean }
-    o - Object[] where is o[0] is one element array wis supposed result from data
-                        o[1] is result of checking method
-                        o[2] is array pf arguments
-
-    this example is works well:
-    o -> {          Object[] arr = (Object[]) o;
-                    *returnType* expectedValue = (*returnType*) ((Object[]) arr[0])[0];
-                    *returnType* resultValue = (*returnType*) arr[1];
-                    return resultValue.equals(expectedValue);
-                }
-    or any your logic returns boolean that means is method works right
+    (4) Checker: new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] paramSet) {
+                        return (int) expVal == (int) resVal;
+                    }
+    check arguments:    resVal - result of checking method
+                        expVal - expected value (may no exist if don't written in (5))
+                        argSet - array of arguments
 
 
-    (5) *AnyObjectType*[][][]: test data
-                {{expected Return Value (if needed)},  {parameters...}},{...},{...}
+    (5) *Object*[][][]: test data
+                {{expected Return Value (if needed)},  {parameters...}},
+                {{.}, {...}},
+                {{.}, {...}} ...
 
      */
 
-
-
+    //EXAMPLE TASK
+//    @TaskAnnot
+//    public Task myTask() {
+//        return new Task("sum", int.class, new Class[]{int.class, int.class},
+//                new Checker() {
+//                    @Override
+//                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+//                        int a = (int) argSet[0];
+//                        int b = (int) argSet[1];
+//
+//                        return a + b == (int) resVal;
+//
+//                        //or just return (int) resVal == (int) expVal;
+//                    }
+//                }, new Object[][][]{
+//                {{4}, {2, 2}},
+//                {{6}, {3, 3}},
+//                {{100}, {73, 27}}
+//        });
+//    }
 
     @TaskAnnot
     public Task task1() {
         Task currentTask = new Task("calculate", int.class, new Class[]{int.class, int.class, int.class, int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    int expectedValue = (int) ((Object[]) arr[0])[0];
-
-                    int resultValue = (int) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
+                new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+                        return (int) expVal == (int) resVal;
+                    }
                 }, new Integer[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
                 {{15}, {5, 1, 4, 2}},
                 {{21}, {3, 2, 15, 3}}
         });
@@ -82,19 +97,12 @@ public class TaskList {
     @TaskAnnot
     public Task task2() {
         Task currentTask = new Task("calculate", float.class, new Class[]{float.class, float.class, float.class, float.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    float expectedValue = (float) ((Object[]) arr[0])[0];
-
-                    float resultValue = (float) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
+                new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+                        return (float) expVal == (float) resVal;
+                    }
                 }, new Float[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
                 {{13.5f}, {3f, 2f, 5f, 2f}},
                 {{17f}, {4f, 0.5f, 15f, 4f}}
         });
@@ -108,19 +116,12 @@ public class TaskList {
     @TaskAnnot
     public Task task3() {
         return new Task("checkTwoNumbers", boolean.class, new Class[]{int.class, int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
-
-                    boolean resultValue = (boolean) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
+                new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+                        return (boolean) expVal == (boolean) resVal;
+                    }
                 }, new Object[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
                 {{true}, {7, 9}},
                 {{false}, {1, 3}},
                 {{false}, {15, 30}}
@@ -130,45 +131,54 @@ public class TaskList {
     @TaskAnnot
     public Task task4() {
         return new Task("isNegative", boolean.class, new Class[]{int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
-
-                    boolean resultValue = (boolean) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
+                new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+                        return (boolean) resVal == ((int) argSet[0] < 0);
+                    }
                 }, new Object[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
-                {{false}, {7}},
-                {{true}, {-5}},
-                {{false}, {0}}
+                {{}, {7}},
+                {{}, {-5}},
+                {{}, {0}}
         });
     }
 
     @TaskAnnot
     public Task task5() {
         return new Task("isLeapYear", boolean.class, new Class[]{int.class},
-                o -> {
-                    //prepare data
-                    Object[] arr = (Object[]) o;
-
-                    boolean expectedValue = (boolean) ((Object[]) arr[0])[0];
-
-                    boolean resultValue = (boolean) arr[1];
-                    //
-                    //there is checks
-                    return resultValue == expectedValue;
+                new Checker() {
+                    @Override
+                    public boolean check(Object resVal, Object expVal, Object[] argSet) {
+                        return (boolean) expVal == (boolean) resVal;
+                    }
                 }, new Object[][][]{
-                //test data
-                //{{expected Return Value},  {parameters...}},{...},{...}
                 {{true}, {2000}},
                 {{false}, {2001}},
                 {{false}, {2600}}
         });
+    }
+
+
+    public static List<Task> getTasks() {
+        TaskList exemplar = new TaskList();
+
+        List<Task> list = new ArrayList<>();
+
+        Class<TaskList> taskListClass = TaskList.class;
+
+        Method[] tasksMethods = taskListClass.getMethods();
+        for (Method m : tasksMethods) {
+            if (m.getAnnotation(TaskAnnot.class) != null) {
+                try {
+                    list.add((Task) m.invoke(exemplar));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    System.err.println("Error preparing Task " + m.getName() + " :" + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return list;
     }
 
 }
